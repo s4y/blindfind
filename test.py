45bfbd4aa0f0b2d3e9be1966be59f647cebaba92
Sidney San Martín <s@sidneysm.com>
2012-03-24 18:02:01 -0400

    Too late for good commits

--- test.py
+++ test.py
@@ -5,17 +5,19 @@ window = pyglet.window.Window(width=670, height=669)
 
 @window.event
 def on_mouse_motion(mouseX, mouseY, dx, dy):
-	x = (mouseX - window.width / 2) / 4.0
-	y = (mouseY - window.height / 2) / 4.0
+	x = (mouseX - window.width / 2) / 20.0
+	y = (mouseY - window.height / 2) / 20.0
 
 	print("x: %s, y: %s" % (x, y))
+
 	player.position = (x, y, 0)
 
 @window.event
 def on_draw():
 	window.clear()
 	sprite.set_position(0, 0)
-	sprite.draw()
+	pyglet.text.Label('DING', font_name='Arial', font_size=24, x=100, y=100).draw()
+	image.blit(10, 10)
 
 
 event_loop = pyglet.app.EventLoop()
@@ -29,6 +31,10 @@ def on_enter():
 	player.play()
 
 	image = pyglet.image.load('crosshairs.png')
-	sprite = pyglet.sprite.Sprite(image)
+	sprite = pyglet.sprite.Sprite(image, x=200, y=200)
+	sprite.opacity = 1
 
+pyglet.gl.glClearColor(1, 1, 1, 1)
+pyglet.gl.glEnable(pyglet.gl.GL_BLEND)
+pyglet.gl.glBlendFunc(pyglet.gl.GL_SRC_ALPHA, pyglet.gl.GL_ONE_MINUS_SRC_ALPHA)
 event_loop.run()
